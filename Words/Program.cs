a5cf3ba416a07c72ef8e216e0941cb2d527fbfe4
Тигран Мартиросян <tomartirosyan@edu.hse.ru>
2018-05-13 02:28:55 +0300

    cool

---
+++ Words/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CoolSR
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string info;
+            Errors res = CheckInput(args,out info);
+            if (res != Errors.Ok)
+            {
+                switch (res)
+                {
+                    case Errors.ConflictingFlags:
+                        Console.WriteLine("ConFlictFlags");
+                        break;
+                    case Errors.CannotOpen:
+                        Console.WriteLine("CAN NOT OPEN");
+                        break;
+                    case Errors.EmptyInput:
+                        Console.WriteLine("Empty Input.");
+                        break;
+                    case Errors.InvalidFlags:
+                        Console.WriteLine("Invalid Flag:"+info);
+                        break;
+                    case Errors.Ok:
+                        break;
+                }
+            }
+
+
+
+        }
+
+
+        static string[] arguments = { "-q", "-s", "-c", "-p", "-l" };
+
+        static Errors CheckInput(string[] args, out string info)
+        {
+            info = "";
+            if (args.Length == 0) return Errors.EmptyInput;
+
+            //Check if too many files.
+            if (IsManyFiles(args)) return Errors.TooManyFiles;
+            
+            //some stupid checks
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                if (!arguments.Contains(args[i]))
+                {
+                    info = args[i]; return Errors.InvalidFlags;
+                    
+                }
+                if ((args.Contains("-s") || args.Contains("-c")) && args.Contains("-q")) return Errors.ConflictingFlags;
+            }
+            if (args[args.Length - 1][0] == '-')
+            {
+                if (!arguments.Contains(args[args.Length - 1]))
+                {
+                    info = args[args.Length - 1]; return Errors.InvalidFlags;
+
+                }
+            }
+            else
+            {
+                if (!File.Exists(args[args.Length - 1])) return Errors.CannotOpen;
+            }
+
+
+            return Errors.Ok;
+
+        }
+
+        static  bool IsManyFiles(string[] args)
+        {
+            int count = 0;
+            string pattern = @"^\w\S+";
+            for (int i = 0; i < args.Length; i++)
+            {
+                count = Regex.IsMatch(args[i], pattern) ? ++count : count;
+            }
+            if (count >=2) return true;
+
+            return false;
+        }
+    }
+    enum Errors
+    {
+        InvalidFlags,
+        ConflictingFlags,
+        CannotOpen,
+        TooManyFiles,
+        Ok,
+        EmptyInput
+
+    }
+}
---
+++ Words/Text.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text.RegularExpressions;
+
+namespace CoolSR
+{
+    public class Text
+    {
+        private string content;
+
+        private List<string> Words
+        {
+            get
+            {
+                List<string> words = new List<string>();
+                Regex rex = new Regex(@"(?:^|\s)(\S+)");
+                var matches = rex.Matches(content);
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    words.Add(matches[i].Groups[0].Value);
+                }
+                return words;
+            }
+
+        }
+
+        //Произвольное слово состоит только из букв и цифр.
+        private List<string> NormalWords
+        {
+            get
+            {
+                List<string> normwords = new List<string>();
+                Regex rex = new Regex(@"^\w+$");
+                for (int i = 0; i < Words.Count; i++)
+                {
+                    if(rex.IsMatch(Words[i])) normwords.Add(Words[i]);
+                }
+                return normwords;
+            }
+        }
+
+        private List<string> RealWords
+        {
+            get
+            {
+                List<string> realwords = new List<string>();
+                Regex rex = new Regex(@"^[^\d\W]+$");
+                for (int i = 0; i < Words.Count; i++)
+                {
+                    if (rex.IsMatch(Words[i])) realwords.Add(Words[i]);
+                }
+                return realwords;
+            }
+        }
+
+        private List<string> CapitalWords
+        {
+            get
+            {
+                List<string> capwords = new List<string>();
+                Regex rex = new Regex(@"^[[:upper:]]?[[:lower:]]+$");
+                for (int i = 0; i < Words.Count; i++)
+                {
+                    if (rex.IsMatch(Words[i])) capwords.Add(Words[i]);
+                }
+                return capwords;
+            }
+        }
+
+        public List<string> LongestWords
+        {
+            get
+            {
+                List<string> longestWords = new List<string>();
+                int max = Words.Max(x => x.Length);
+                for (int i = 0; i < Words.Count; i++)
+                {
+                 if(Words[i].Length==max) longestWords.Add(Words[i]);
+                }
+                return longestWords;
+            }
+        }
+        public int LongestWordLength {
+            get { return Words.Max(x => x.Length); }
+        }
+
+
+        public Text(string content)
+        {
+            this.content = content;
+        }
+
+
+        public void DeleteDupSpaces()
+        {
+            Regex rex = new Regex(@"\s\s+");
+            rex.Replace(content, " ");
+
+        }
+    }
+}
