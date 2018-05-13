3cdb7ee36804d099cf491a9a0d05abd93ba48151
Тигран Мартиросян <tomartirosyan@edu.hse.ru>
2018-05-14 01:30:25 +0300

    куфвн

--- Words/Program.cs
+++ Words/Program.cs
@@ -5,16 +5,24 @@ using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+/*
+Выполнил: Мартиросян Тигран
+Группа 176(1)
+*/
 namespace CoolSR
 {
     class Program
     {
+        static string[] arguments = { "-q", "-s", "-c", "-p", "-l" };
 
         static void Main(string[] args)
         {
+            //Переведем все в нижниый регистр. Для имени файла это не критично, очевидно почему.
+            args= Array.ConvertAll(args, x => x.ToLower());
             string info;
-            Errors res = CheckInput(args,out info);
+            //посмотрим какой ответ нам дала проверка аргументов командной строки
+            Errors res = CheckInput(args, out info);
+            //Если что-то плохо, то скажем об этом пользователю и выйдем.
             if (res != Errors.Ok)
             {
                 switch (res)
@@ -29,40 +37,171 @@ namespace CoolSR
                         Console.WriteLine("Empty Input.");
                         break;
                     case Errors.InvalidFlags:
-                        Console.WriteLine("Invalid Flag:"+info);
-                        break;
-                    case Errors.Ok:
+                        Console.WriteLine("Invalid Flag:" + info);
                         break;
                 }
+                return;
+            }
+            //Ура. Все хорошо, посмотрим указал ли пользовател имя файла.
+            if (args[args.Length - 1][0] == '-')
+            {
+                //Если указал то пойдем работать с консолью
+                Start(args, InputKind.Console);
+            }
+            else
+            {
+                //Если не указал пойдем работать с файлом
+                Start(args, InputKind.File);
             }
 
-
+            Console.ReadKey();
 
         }
 
+        /// <summary>
+        /// Выполняет основную работу программы.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="inputKind"></param>
+        private static void Start(string[] args, InputKind inputKind)
+        {
+            Text text;
+            Text output;
+            //Чекаем способ ввода, получчаем текст в зависисости от результата проверки
+            if (inputKind == InputKind.Console)
+            {
+                Console.WriteLine("Введите текст:");
+                string userinput = Console.ReadLine();
+                text = new Text(userinput);
+            }
+            else
+            {
+                string input = File.ReadAllText(args[args.Length - 1]);
+                text = new Text(input);
+            }
+            //Исходящий текст. 
+            output = new Text(text);
+            //Выполняем просьбы пользователя исходя из параметров
 
-        static string[] arguments = { "-q", "-s", "-c", "-p", "-l" };
+            if (args.Contains("-l")) { PrintLengthInfos(text); }
 
+
+            if (args.Contains("-s"))
+            {
+                output.DeleteDupSpaces();
+            }
+
+            if (args.Contains("-c"))
+            {
+                output.OnlyRealWords();
+            }
+
+            if (args.Contains("-p"))
+            {
+                PrintStatistics(text, output);
+            }
+            //Если пользователь не просил новый файл то его делать не будет
+            if (!args.Contains("-q"))
+            {
+                var sw = File.CreateText("text.txt");
+                sw.Write(output.Content);
+                sw.Close();
+            }
+        }
+
+        /// <summary>
+        /// Печатает статистику в консоль, а также возвращает в виде string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        private static string PrintStatistics(Text input, Text output)
+        {
+            string str = "";
+            str += $"\nСимволы: входной:{input.Length}; выходной: {output.Length}";
+            str += $"\nСтроки: входной:{input.LinesCount}; выходной: {output.LinesCount}";
+            str += $"\nСлова: входной:{input.Words.Count}; выходной: {output.Words.Count}";
+            str += $"\nПроизвольные слова: входной {input.NormalWords.Count};";
+            str += $"\nНастоящие слова: входной {input.RealWords.Count};";
+            str += $"\nЗаглавные слова: входной {input.CapitalWords.Count};";
+            str += $"\nАкронимы: входной {input.Acronyms.Count};";
+            Console.WriteLine(str);
+            return str;
+        }
+
+        /// <summary>
+        /// Печатает информацию по длинам в консоль, а также возвращает в виде string
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static string PrintLengthInfos(Text text)
+        {
+            string str = "";
+            if (text.LongestWords.Count != 0)
+            {
+                var longestWords = text.LongestWords;
+                str += $"\nСлово(длина {longestWords[0].Length}): ";
+                for (int i = 0; i < longestWords.Count; i++)
+                {
+                    str += longestWords[i] + "; ";
+                }
+            }
+
+            if (text.LongestNormalWords.Count != 0)
+            {
+                var longestnormalWords = text.LongestNormalWords;
+                str += $"\nПроизвольное слово(длина {longestnormalWords[0].Length}): ";
+                for (int i = 0; i < longestnormalWords.Count; i++)
+                {
+                    str += longestnormalWords[i] + "; ";
+                }
+            }
+
+            if (text.LongestRealWords.Count != 0)
+            {
+                var longetRealWords = text.LongestRealWords;
+                str += $"\nНастоящее слово(длина {longetRealWords[0].Length}): ";
+                for (int i = 0; i < longetRealWords.Count; i++)
+                {
+                    str += longetRealWords[i] + "; ";
+                }
+            }
+            Console.WriteLine(str);
+            return str;
+        }
+
+        
+        /// <summary>
+        /// Проверяет введенные пользователем параметры на корректность.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="info"></param>
+        /// <returns></returns>
         static Errors CheckInput(string[] args, out string info)
         {
             info = "";
+            //здесь все понятно
             if (args.Length == 0) return Errors.EmptyInput;
 
-            //Check if too many files.
+            //Проверяем много ли файлов
             if (IsManyFiles(args)) return Errors.TooManyFiles;
-            
-            //some stupid checks
+
+            //Проверяем все аргументы кроме последнего
             for (int i = 0; i < args.Length - 1; i++)
             {
+                //Проверяем несуществующие аргументы
                 if (!arguments.Contains(args[i]))
                 {
                     info = args[i]; return Errors.InvalidFlags;
-                    
                 }
+                //проверяем несовместимые аргументы
                 if ((args.Contains("-s") || args.Contains("-c")) && args.Contains("-q")) return Errors.ConflictingFlags;
             }
+            //Проверяем последний аргумент отдельно.
+            //Если первый символ "-" то это аргумент, а не имя файла
             if (args[args.Length - 1][0] == '-')
             {
+
                 if (!arguments.Contains(args[args.Length - 1]))
                 {
                     info = args[args.Length - 1]; return Errors.InvalidFlags;
@@ -71,6 +210,7 @@ namespace CoolSR
             }
             else
             {
+                //Проверим существвуует ли файл с таким имененем
                 if (!File.Exists(args[args.Length - 1])) return Errors.CannotOpen;
             }
 
@@ -79,7 +219,12 @@ namespace CoolSR
 
         }
 
-        static  bool IsManyFiles(string[] args)
+        /// <summary>
+        /// Возвращает True если среди аргументов найдено более одного имени файла.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static bool IsManyFiles(string[] args)
         {
             int count = 0;
             string pattern = @"^\w\S+";
@@ -87,11 +232,12 @@ namespace CoolSR
             {
                 count = Regex.IsMatch(args[i], pattern) ? ++count : count;
             }
-            if (count >=2) return true;
+            if (count >= 2) return true;
 
             return false;
         }
     }
+    //Было бы лучше сделать словарь, но и так неплохо.
     enum Errors
     {
         InvalidFlags,
@@ -101,5 +247,11 @@ namespace CoolSR
         Ok,
         EmptyInput
 
+    }
+
+    enum InputKind
+    {
+        Console,
+        File
     }
 }
--- Words/Text.cs
+++ Words/Text.cs
@@ -1,74 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace CoolSR
 {
     public class Text
     {
+        /// <summary>
+        /// Поле содержит в себе текст.
+        /// </summary>
         private string content;
+        /// <summary>
+        /// Поле содержит в себе список слов.
+        /// </summary>
+        private List<string> words;
+        /// <summary>
+        /// Поле содержит в себе список произвольных слов.
+        /// </summary>
+        private List<string> normalWords;
+        /// <summary>
+        /// Поле содержит список настоящих слов
+        /// </summary>
+        private List<string> realWords;
 
-        private List<string> Words
+        /// <summary>
+        /// Возвращает содержимое
+        /// </summary>
+        public string Content => content;
+
+        /// <summary>
+        /// Возвращает количество символов.
+        /// </summary>
+        public int Length => content.Length;
+
+        /// <summary>
+        /// возвращает количество строк.
+        /// </summary>
+        public int LinesCount
+        {
+            get { return content.Split('\n').Length; }
+        }
+
+        #region Слово
+        /// <summary>
+        /// Возвращает или задает список слов.
+        /// </summary>
+        public List<string> Words
         {
             get
             {
-                List<string> words = new List<string>();
-                Regex rex = new Regex(@"(?:^|\s)(\S+)");
-                var matches = rex.Matches(content);
-                for (int i = 0; i < matches.Count; i++)
-                {
-                    words.Add(matches[i].Groups[0].Value);
-                }
                 return words;
             }
-
+            private set { words = value; }
         }
 
-        //Произвольное слово состоит только из букв и цифр.
-        private List<string> NormalWords
+        /// <summary>
+        /// Находит слова из content и возвращает в виде списка.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetWords()
         {
-            get
+            List<string> words = new List<string>();
+            Regex rex = new Regex(@"(?:^|\s)(\S+)");
+            var matches = rex.Matches(content);
+            for (int i = 0; i < matches.Count; i++)
             {
-                List<string> normwords = new List<string>();
-                Regex rex = new Regex(@"^\w+$");
-                for (int i = 0; i < Words.Count; i++)
-                {
-                    if(rex.IsMatch(Words[i])) normwords.Add(Words[i]);
-                }
-                return normwords;
+                words.Add(matches[i].Groups[1].Value);
             }
+            return words;
         }
 
-        private List<string> RealWords
-        {
-            get
-            {
-                List<string> realwords = new List<string>();
-                Regex rex = new Regex(@"^[^\d\W]+$");
-                for (int i = 0; i < Words.Count; i++)
-                {
-                    if (rex.IsMatch(Words[i])) realwords.Add(Words[i]);
-                }
-                return realwords;
-            }
-        }
-
-        private List<string> CapitalWords
-        {
-            get
-            {
-                List<string> capwords = new List<string>();
-                Regex rex = new Regex(@"^[[:upper:]]?[[:lower:]]+$");
-                for (int i = 0; i < Words.Count; i++)
-                {
-                    if (rex.IsMatch(Words[i])) capwords.Add(Words[i]);
-                }
-                return capwords;
-            }
-        }
-
+        /// <summary>
+        /// Возвращает список из слов максимальной длины
+        /// </summary>
         public List<string> LongestWords
         {
             get
@@ -77,27 +85,178 @@ namespace CoolSR
                 int max = Words.Max(x => x.Length);
                 for (int i = 0; i < Words.Count; i++)
                 {
-                 if(Words[i].Length==max) longestWords.Add(Words[i]);
+                    if (Words[i].Length == max) longestWords.Add(Words[i]);
                 }
                 return longestWords;
             }
         }
-        public int LongestWordLength {
-            get { return Words.Max(x => x.Length); }
+
+        #endregion
+
+        #region Произвольное слово
+
+        /// <summary>
+        /// Вовзращает или задает список Произвольных слов.
+        /// </summary>
+        public List<string> NormalWords
+        {
+            get { return normalWords; }
+            private set { normalWords = value; }
+        }
+        /// <summary>
+        /// Находит произвольные слова из списка слов и возвращает в виде списка.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetNormalWords()
+        {
+            List<string> normwords = new List<string>();
+            Regex rex = new Regex(@"^\w+$");
+            for (int i = 0; i < Words.Count; i++)
+            {
+                if (rex.IsMatch(Words[i])) normwords.Add(Words[i]);
+            }
+            return normwords;
+        }
+        /// <summary>
+        /// Возвращает список из произвольных слов максимальной длины
+        /// </summary>
+        public List<string> LongestNormalWords
+        {
+            get
+            {
+                List<string> longestWords = new List<string>();
+                int max = NormalWords.Max(x => x.Length);
+                for (int i = 0; i < NormalWords.Count; i++)
+                {
+                    if (NormalWords[i].Length == max) longestWords.Add(NormalWords[i]);
+                }
+                return longestWords;
+            }
+        }
+        #endregion
+
+        #region Настоящее слово
+
+
+
+        /// <summary>
+        /// Возвращает или задает список Настоящих слов.
+        /// </summary>
+        public List<string> RealWords
+        {
+            get { return realWords; }
+            private set { realWords = value; }
+        }
+        /// <summary>
+        /// Возвращает список настоящих слов из списка слов в виде списка.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetRealWords()
+        {
+            List<string> realwords = new List<string>();
+            Regex rex = new Regex(@"^[^\d\W_]+$");
+            for (int i = 0; i < Words.Count; i++)
+            {
+                if (rex.IsMatch(Words[i])) realwords.Add(Words[i]);
+            }
+            return realwords;
+        }
+
+        /// <summary>
+        /// Возвращает список слов максимальной длины.
+        /// </summary>
+        public List<string> LongestRealWords
+        {
+            get
+            {
+                List<string> longestWords = new List<string>();
+                int max = RealWords.Max(x => x.Length);
+                for (int i = 0; i < RealWords.Count; i++)
+                {
+                    if (RealWords[i].Length == max) longestWords.Add(RealWords[i]);
+                }
+                return longestWords;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Возврщает список заглавных слов. из списка настоязих слов
+        /// </summary>
+        public List<string> CapitalWords
+        {
+            get
+            {
+                List<string> capwords = new List<string>();
+                Regex rex = new Regex(@"^[[:upper:]]?[[:lower:]]+$");
+                for (int i = 0; i < RealWords.Count; i++)
+                {
+                    if (rex.IsMatch(RealWords[i])) capwords.Add(RealWords[i]);
+                }
+                return capwords;
+            }
+        }
+        /// <summary>
+        /// Возвращает список акронимов
+        /// </summary>
+        public List<string> Acronyms
+        {
+            get
+            {
+                List<string> acronyms = new List<string>();
+                Regex rex = new Regex(@"^(?:[A-Z]){2,}$");
+                for (int i = 0; i < RealWords.Count; i++)
+                {
+                    if (rex.IsMatch(RealWords[i])) acronyms.Add(RealWords[i]);
+                }
+                return acronyms;
+            }
+        }
+
+        public Text(Text text) : this(text.content)
+        {
+
         }
 
 
         public Text(string content)
         {
             this.content = content;
+            Words = GetWords();
+            NormalWords = GetNormalWords();
+            RealWords = GetRealWords();
+            
         }
 
-
+        /// <summary>
+        /// Удаляет повторяющиеся непечатные символы, заменяет на один пробел
+        /// </summary>
         public void DeleteDupSpaces()
         {
             Regex rex = new Regex(@"\s\s+");
-            rex.Replace(content, " ");
-
+            content = rex.Replace(content, " ");
+            Words = GetWords();
+            NormalWords = GetNormalWords();
+            RealWords = GetRealWords();
         }
+        /// <summary>
+        /// Оставляет только настоящие слова и пробел следюущий за ним.
+        /// </summary>
+        public void OnlyRealWords()
+        {
+            string newcontent = "";
+            for (int i = 0; i < RealWords.Count; i++)
+            {
+                newcontent += RealWords[i] + " ";
+            }
+
+            content = newcontent;
+            Words = GetWords();
+            NormalWords = GetNormalWords();
+            RealWords = GetRealWords();
+        }
+
+       
     }
 }
